f43143784a4ff876ee5fd5c5a3eaced1e1def9ba
libai204 <125873679+libai204@users.noreply.github.com>
2023-04-04 17:09:53 +0800

    Add files via upload

---
+++ pages/notfound/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div style="font-size: 30px">
+    功能正在开发中！
+  </div>
+</template>
+<script>
+export default {
+  props:{
+
+  },
+  components:{
+
+  },
+  data(){
+    return {
+      data(){
+        
+      }
+    }
+  },
+  computed:{
+
+  },
+  methods:{
+
+  },
+  created(){
+
+  },
+  mounted(){
+
+  }
+}
+</script>
+<style scoped lang="">
+
+</style>
